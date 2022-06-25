74eb552318b00d9890c67faeaeb8f8378ae77b2c
NandiniKriplani <78662744+NandiniKriplani@users.noreply.github.com>
2022-06-25 22:53:43 +0530

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -8,7 +8,7 @@ module.exports =
   networks: {
     goerli: {
       url: ' https://eth-goerli.alchemyapi.io/v2/6NpC3XygFnkJYUoesT6luWutdivO5VoH',
-      accounts: ['85aca8ed8d1de64657b0223af44f4a9f01fcf3c3234ba9579cd5c2117039a3e8']
+      accounts: ['']
     }
    
   }
